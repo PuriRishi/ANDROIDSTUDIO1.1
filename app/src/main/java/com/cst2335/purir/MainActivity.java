4b634bc29f7b3b83de95eeb4dc20b94cff5e28f9
Rishabh Puri <puri0023@algonquinlive.com>
2023-01-28 17:42:27 -0500

    lab 2.1 linear layout

--- app/src/main/java/com/cst2335/purir/MainActivity.java
+++ app/src/main/java/com/cst2335/purir/MainActivity.java
@@ -3,6 +3,8 @@ package com.cst2335.purir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        String toastmessage = MainActivity.this.getString(R.string.toast_message);
+        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, toastmessage, Toast.LENGTH_SHORT).show());
+
     }
 }
